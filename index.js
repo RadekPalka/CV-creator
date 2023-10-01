f049de341bb7bd4a98fb69be6c93444fd1f2a9fa
Radoslaw Palka <radekpalka@gmail..com>
2023-10-01 09:53:16 +0200

    create homepage

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 const app = express()
 
 app.get("/", (req, res) =>{
-  res.sendFile(path.join(__dirname + "views/index.html"))
+  res.sendFile(path.join(__dirname + "/views/index.html"))
 })
 
 app.listen(port)
