2d78b87705e28adbd1a6073de5e6b1cd8d4e3fba
Radoslaw Palka <radekpalka@gmail..com>
2023-10-01 09:26:46 +0200

    create server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const path = require("path")
+const port = 3000
+const app = express()
+
+app.get("/", (req, res) =>{
+  res.sendFile(path.join(__dirname + "views/index.html"))
+})
+
+app.listen(port)
